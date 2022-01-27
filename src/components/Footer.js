004eee8d39919c19c1350bcce87ecc04b6b0912a
Jared Bradley <jaredbradley@jareds-mbp.lan>
2022-01-27 09:05:10 -0600

    deployed front end/add info to footer w/styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,18 @@
 const Footer = (props) => {
-    return <h1>Footer</h1>
+    return (
+        <footer>
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"></link>
+            <div className="footerName">
+            <h5>Jared Bradley <span>&copy;</span></h5>
+            </div>
+            <div className="footerGithub">
+            <h3><a href="https://github.com"><i class="fab fa-github"></i></a></h3>
+            </div>
+            <div className="footerLinkedIn">
+                <h3><a href="https://www.linkedin.com/in/jaredbradley95/"><i class="fab fa-linkedin"></i></a></h3>
+            </div>
+        </footer>
+    )
 };
 
 export default Footer;
