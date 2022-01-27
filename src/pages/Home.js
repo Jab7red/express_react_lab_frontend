2a80b87ce2b00cbb9db4ba24fb0723bb61b47650
Jared Bradley <jaredbradley@jareds-mbp.lan>
2022-01-27 08:22:42 -0600

    add info to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,10 @@
 const Home = (props) => {
-    return <h1>Home</h1>
+    return (
+        <div>
+            <h1>Hello and Welcome to my Portfolio</h1>
+            <h4>Please see the about page for info about me <br /> and also click the projects link to see my recent projects.</h4>
+        </div>
+    )
 };
 
 export default Home;
